3ed16610c12ea8df694ba06118fd664598c5b3cf
andrew blinn <me@disconcision.com>
2018-10-26 23:06:52 -0400

    read list to graph done

--- ShareVis.hs
+++ ShareVis.hs
@@ -16,14 +16,76 @@ import Control.Parallel.Strategies
 import System.Mem.StableName
 import System.IO.Unsafe
 
+import qualified Data.GraphViz as G
+import qualified Data.GraphViz.Attributes.Complete as G
+import qualified Data.GraphViz.Types as G
 
-a = 5
-b = 6
-c = b
 
-l0 = 0 : []
-l1 = 1 : l0
-l2 = tail l1
+
+
+
+type ID = Int
+type Node = ID 
+
+type V = Node
+type E = (Node,Node)
+type Graph = ([V], [E])
+
+--toGraph :: [String] -> Graph
+--toGraph [] = ([("null", 0)],[])
+
+type MemoryTable = [(ID, (String, ID))]
+
+--initialMemoryTable :: MemoryTable
+--initialMemoryTable = [(0, ("/", 0))]
+
+
+lx = []
+l0 = "0" : lx
+l1 = "1" : l0
+l2 = "2" : l1
+
+
+l0name :: String
+l0name = "l0"
+l1name = "l1"
+l2name = "l2"
+
+testLists :: [(String, [String])]
+testLists = [(l0name, l0), (l1name, l1), (l2name, l2)]
+
+
+variableTable :: MemoryTable
+variableTable = concatMap
+  (\ (variableName, list) ->
+      [(getName variableName, (variableName, getName list))]
+    ) testLists  
+
+memoryTable :: MemoryTable
+memoryTable = mapMem l2 []
+
+mapMem :: [String] -> MemoryTable -> MemoryTable
+mapMem [] _ = [(getName [], ("null", getName []))]
+mapMem c@(x: xs) table =
+    concat [[(getName c, (x, getName xs))], (mapMem xs table)]
+
+graph :: Graph
+graph = (map (\ (loc, (label, next)) -> loc) memoryTable,
+         map (\ (loc, (label, next)) -> (loc, next)) memoryTable)
+
+main = do
+    print $ concat [memoryTable, variableTable]
+    print graph
+    --print $ getName l0
+    --print $ getName l1
+    --print $ getName l2
+
+
+
+
+
+
+
 
 getName :: a -> Int
 -- TODO: understand why we need bangpatter and unsafePerformIO
@@ -31,10 +93,10 @@ getName :: a -> Int
 getName !d = unsafePerformIO $ do
     n <- makeStableName $! d
     return $ hashStableName n
-
+{-
 main = do
     print $ getName l0
     print $ getName l1
     print $ getName l2
     --print $ getName c
-        
+-}
