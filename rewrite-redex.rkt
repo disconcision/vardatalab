65d41f36d1e4674263f4f36f98de977504119a13
andrew blinn <me@disconcision.com>
2019-03-04 17:36:16 -0500

    cleanup

--- rewrite-redex.rkt
+++ rewrite-redex.rkt
@@ -7,40 +7,35 @@
 
 (define-language L
   (e (e e)
-     (λ (x t) e)
      (λ (x t) -> e)
      (λ (x t) (x t) -> e)
      x
+     number
      (apply e e)
      (mkVarT e)
      foo
      bar
-     +
-     number)
+     +)
   (t (→ t t)
      (Var t)
      Int)
   (x variable-not-otherwise-mentioned))
 
-#;(redex-match
-   L
-   e
-   (term (λ (x Int) x)))
 
 #;(redex-match
    L
    (e_1 e_2)
-   (term ((λ (x Int) (apply x 1))
+   (term ((λ (x Int) -> (apply x 1))
           (+ 1 2))))
 
 (define-extended-language L+Γ L
   [Γ · (x : t Γ)])
 
-(define-metafunction L+Γ
+#; (define-metafunction L+Γ
   [(different x_1 x_1) #f]
   [(different x_1 x_2) #t])
 
-(define-judgment-form
+#;(define-judgment-form
   L+Γ
   #:mode (types I I O)
   #:contract (types Γ e t)
@@ -93,23 +88,23 @@
 
 
 
-(judgment-holds
+#;(judgment-holds
  (types · 1 t)
  t)
 
-(judgment-holds
+#;(judgment-holds
  (types · (λ (x Int) 4) t)
  t)
 
-(judgment-holds
+#;(judgment-holds
  (types · (mkVarT 5) t)
  t)
 
-(judgment-holds
+#;(judgment-holds
  (types · (apply (mkVarT (λ (x Int) 4)) (mkVarT 5)) t)
  t)
 
-(judgment-holds
+#;(judgment-holds
  (types · (apply (mkVarT (λ (x Int) (y Int) -> 4)) (mkVarT 5)) t)
  t)
 
@@ -135,10 +130,10 @@
   (reduction-relation
    Ev
    #:domain e
+   
    (--> (in-hole E (apply (mkVarT (λ (x t) -> x)) e_1))
         (in-hole E e_1)
         "v1")
-   
    (--> (in-hole E (apply (apply (mkVarT (λ (x_1 t_1) (x_2 t_2) -> x_1)) e_1) e_2))
         (in-hole E e_1)
         "v1a")
@@ -153,14 +148,11 @@
         (in-hole E (mkVarT e_1))
         "v2a")
    
-   #;(--> (in-hole E (apply (mkVarT (λ (x t) -> (e_1 e_2))) e_3))
-          (in-hole E (apply (apply (mkVarT (λ (x t) -> e_1)) e_3) (apply (mkVarT (λ (x t) -> e_2)) e_3)))
-          "v3a")
    (--> (in-hole E (apply (apply (mkVarT (λ (x_1 t_1) (x_2 t_2) -> (e_1 e_2))) e_11) e_22))
         (in-hole E (apply (apply (apply (mkVarT (λ (x_1 t_1) (x_2 t_2) -> e_1)) e_11) e_22)
                         (apply (apply (mkVarT (λ (x_1 t_1) (x_2 t_2) -> e_2)) e_11) e_22)))
-        "v3b")
-   #;(--> )))
+        "v3")
+   ))
 
 
 #;(traces red
